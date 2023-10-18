df5c1d0a6f375843fcc88e6cbad55452504b759a
Wonka <willie.lima_lvs@hotmail.com>
2023-10-18 20:19:22 -0300

    Commit de resolução da abstração do celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456", "modelo 1", "1a1a1a1a", 64);
+nokia.Ligar(12345678);
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("654321", "modelo 2", "a1a1a1a1a1", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
